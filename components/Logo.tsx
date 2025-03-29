d8cb02385192caad4c05540e74e475179f34ec40
justjohnashley <pural.johnashleyc@gmail.com>
2025-03-29 21:38:04 +0800

    header & sidemenu

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,10 +4,24 @@ import React from 'react'
 
 const Logo = ({className}:{className?:string}) => {
   return (
-  <Link href={"/"}>
-    <h2 className={cn("text-2xl text-shop_light_yellow font-black tracking-wider uppercase hover:text-shop_dark_green hoverEffect group font-sans",className)}>
+  <Link href={"/"} className={cn("relative flex items-center gap-2 group w-32 h-12", className)}>
+    {/*<h2 className={cn("text-2xl text-shop_light_yellow font-black tracking-wider uppercase hover:text-shop_dark_green hoverEffect group font-sans",className)}>
         <span className='text-4xl text-shop_yellow group-hover:text-shop_light_green hoverEffect'>P</span>iñana
-    </h2>
+    </h2>*/}
+
+     {/* Default Image */}
+     <img
+        src="/images/web-yellow.png"
+        alt="Piñana"
+        className={cn("absolute inset-0 w-full h-full object-contain hoverEffect opacity-100 group-hover:opacity-0",className)}
+      />
+
+      {/* Hover Image */}
+      <img
+        src="/images/web-green.png"
+        alt="Piñana Gourmet"
+        className={cn("absolute inset-0 w-full h-full object-contain hoverEffect opacity-0 group-hover:opacity-100",className)}
+      />
     </Link>
     
 )
---
+++ components/SocialMedia.tsx
@@ -0,0 +1,63 @@
+
+import React from 'react'
+import { PiFacebookLogoFill, PiInstagramLogoFill, PiTiktokLogoFill } from "react-icons/pi";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "./ui/tooltip";
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+
+interface Props {
+    className?: string;
+    iconClassName?: string;
+    tooltipClassName?: string;
+}
+
+const socialLink = [
+    {
+      title: "Instagram",
+      href: "https://www.instagram.com/pinyana.gourmet",
+      icon: <PiInstagramLogoFill className="w-5 h-5" />,
+    },
+    {
+      title: "Facebook",
+      href: "https://www.facebook.com/pinyanagourmet/",
+      icon: <PiFacebookLogoFill className="w-5 h-5" />,
+    },
+    {
+      title: "Tiktok",
+      href: "https://www.tiktok.com/@pinyanagourmet",
+      icon: <PiTiktokLogoFill className="w-5 h-5" />,
+    },
+];
+  
+
+
+const SocialMedia = ({ className, iconClassName, tooltipClassName }: Props) => {
+  return <TooltipProvider>
+      <div className={cn("flex gap-3.5", className)}>
+          {socialLink?.map((item) => (
+              <Tooltip>
+                  <TooltipTrigger asChild>
+                      <Link
+                          key={item?.title}
+                          target='_blank'
+                          rel='noopener noreferrer'
+                          href={item?.href}
+                          className={cn("p-2 border rounded-full border-shop_light_yellow text-shop_light_yellow hover:bg-shop_light_yellow hover:text-shop_dark_green hoverEffect", iconClassName)}>
+                          {item?.icon}
+                      </Link>
+                  </TooltipTrigger>
+                  <TooltipContent className={cn("ClassValue[]): string text-shop_dark_green bg-shop_light_yellow font-semibold", tooltipClassName)}>
+                      {item?.title}
+                  </TooltipContent>
+            </Tooltip>))}
+          </div>
+      </TooltipProvider>
+  
+}
+
+export default SocialMedia
